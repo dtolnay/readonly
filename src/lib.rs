419d46a51d43dcde316d278e21804bf6c00a78b4
David Tolnay <dtolnay@gmail.com>
2019-03-26 23:15:50 -0300

    Write crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,65 @@
+//! Struct fields that can be read and written from within the same module, but
+//! from outside the module can only be read.
+//!
+//! # Syntax
+//!
+//! Place `#[readonly::make]` on a braced struct or tuple struct. This will make
+//! all fields of the struct publicly readable according to their individual
+//! visibility specifiers, but not writable from other modules.
+//!
+//! ```
+//! mod m {
+//!     #[readonly::make]
+//!     pub struct S {
+//!         pub n: i32,
+//!     }
+//!
+//!     impl S {
+//!         pub fn demo(&mut self) {
+//!             // Can read and write from inside the same module.
+//!             println!("{}", self.n);
+//!             self.n += 1;
+//!         }
+//!     }
+//! }
+//!
+//! fn demo(s: &mut m::S) {
+//!     // From outside the module, can only read.
+//!     println!("{}", s.n);
+//!
+//!     // Does not compile:
+//!     //s.n += 1;
+//! }
+//! ```
+//!
+//! The error appears as follows.
+//!
+//! ```console
+//! error[E0594]: cannot assign to data in a `&` reference
+//!   --> readme.rs:21:5
+//!    |
+//! 21 |     s.n += 1;
+//!    |     ^^^^^^^^ cannot assign
+//! ```
+//!
+//! Optionally, place `#[readonly]` on individual struct fields to make just
+//! those fields publicly readable, without affecting other fields of the
+//! struct.
+//!
+//! ```
+//! # mod m {
+//! #[readonly::make]
+//! pub struct S {
+//!     // This field can be read (but not written) by super.
+//!     #[readonly]
+//!     pub(super) readable: i32,
+//!
+//!     // This field can be neither read nor written by other modules.
+//!     private: i32,
+//! }
+//! # }
+//! ```
+
 extern crate proc_macro;
 
 mod expand;
